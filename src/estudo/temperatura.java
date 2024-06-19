96577f6e90acd3c5917dc86e4f3f987e8d313696
Bruno Ibiapina <brunoibiapina@me.com>
2024-06-19 10:31:04 -0300

    Initial commit

---
+++ src/estudo/temperatura.java
@@ -0,0 +1,22 @@
+package estudo;
+
+import java.util.Locale;
+
+public class temperatura {
+    public static void main(String[] args) {
+        double C, F, K, Re, Ra;
+        C = 2.43;
+
+        F = C * 1.8 + 32;
+        K = C + 273.15;
+        Ra = C * 1.8 + 32 + 459.67;
+        Re = C * 0.8;
+
+        Locale.setDefault(Locale.US);
+        System.out.printf("A temperatura em F é %.2f\n", F);
+        System.out.printf("A temperatura em K é %.2f\n", K);
+        System.out.printf("A temperatira em Ra é %.2f\n", Ra);
+        System.out.printf("A temperatura em Re é %.2f\n", Re);
+
+    }
+}
