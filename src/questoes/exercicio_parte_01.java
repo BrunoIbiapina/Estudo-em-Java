0c4b96c3db5d297fc9714679b01676b7cf0b7f75
Bruno Ibiapina <brunoibiapina@me.com>
2024-06-27 15:54:36 -0300

    entrada

---
+++ src/questoes/exercicio_parte_01.java
@@ -0,0 +1,9 @@
+package questoes;
+
+public class exercicio_parte_01{
+    public static void main(String[] args);
+    // Locale.setDefault(Locale.US);
+
+    System.out.println("Ele tá sem zap");
+
+}
